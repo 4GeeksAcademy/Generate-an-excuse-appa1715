713376ab96f95d965b8246b5601d076cf8d087af
Appa1715 <128950756+Appa1715@users.noreply.github.com>
2023-06-17 03:00:34 +0000

    first java

--- src/app.js
+++ src/app.js
@@ -7,5 +7,26 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.getElementById("#the-excuse").innerHTML = generateExcuse();
+  let generateExcuse = () => {
+    let who = ["Devo", "Non ho potuto", "Mia madre non ha potuto", "La papaia"];
+    let action = ["prendere cura", "fare il cibo", "salutare", ""];
+    let what = ["a mio nipote", "da mia zia", "al Re"];
+    let when = ["stasera.", "ed ancora non mangia", "e devo aiutarla"];
+
+    let whoIndex = Math.floor(Math.random() * who.length);
+    let actionIndex = Math.floor(Math.random() * action.length);
+    let whatIndex = Math.floor(Math.random() * what.length);
+    let whenIndex = Math.floor(Math.random() * when.length);
+
+    return (
+      who[whoIndex] +
+      "" +
+      action[actionIndex] +
+      "" +
+      what[whatIndex] +
+      "" +
+      when[whenIndex]
+    );
+  };
 };
